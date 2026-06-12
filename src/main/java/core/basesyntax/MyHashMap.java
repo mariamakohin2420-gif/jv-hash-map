f08ef16f0e9336a9b83ed1249f637590ddb4341c
Mariia Makohin <maria.makohin2420@gmail.com>
2026-06-12 09:04:03 +0300

    my changes step three

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,6 +4,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
+    private static final int RESIZE_MULTIPLIER = 2;
+
 
     private Node<K, V>[] table;
     private int size;
@@ -72,7 +74,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         Node<K,V>[] oldTable = table;
-        table = new Node[oldTable.length * 2];
+        table = new Node[oldTable.length * RESIZE_MULTIPLIER];
         size = 0;
         for (Node<K,V> currentNode : oldTable) {
             while (currentNode != null) {
